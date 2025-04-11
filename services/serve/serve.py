6ca4fec8612d09100e2996e3e40eccc2d88b5892
AMKCode <aomankit2002@gmail.com>
2025-04-11 23:10:08 +0000

    maybe fix not working bug

--- services/serve/serve.py
+++ services/serve/serve.py
@@ -4,14 +4,24 @@ import json
 import tvm
 from mlc_llm.serve import EngineConfig, PopenServer
 
+subprocess.run(['python', '-m', 'mlc_llm', 'compile',
+                'Llama-3.1-8B-Instruct-q4f16_1-MLC/',
+                '--opt', 'O3',
+                '--overrides', 'disaggregation=1',
+                '-o', 'Llama-3.1-8B-Instruct-q4f16_1-MLC/lib_disagg.so'])
+
 pod_name = os.getenv("POD_NAME", default=None)
 if pod_name is None:
-    print('echo "POD_NAME not found, defaulting CUDA_ID to 0"')
     cuda_id = 0
 else:
     cuda_id = pod_name.split("-")[-1]
-    print(f'echo "setting CUDA_ID to {cuda_id}"')
     cuda_id = int(cuda_id)
+
+num_replicas = os.getenv("NUM_REPLICAS", default=None)
+if num_replicas is None:
+    num_replicas = 1
+else:
+    num_replicas = int(num_replicas)
 
 f_init_nvshmem_uid = tvm.get_global_func("runtime.disco.nvshmem.init_nvshmem_uid")
 uid = list(f_init_nvshmem_uid())
@@ -24,4 +34,14 @@ nvshmem_config = {
 
 os.environ["MLC_NVSHMEM_INIT_CONFIG_JSON_STR"] = json.dumps(nvshmem_config)
 
-subprocess.run(['python', '-m', 'mlc_llm', 'serve', '/Llama-3.1-8B-Instruct-q4f16_1-MLC', '--model-lib', '/Llama-3.1-8B-Instruct-q4f16_1-MLC/lib_disagg.so', '--device', f'cuda:{cuda_id}', '--enable-debug', '--mode', 'server', '--speculative-mode', 'disable', '--prefix-cache-mode', 'disable', '--overrides', 'gpu_memory_utilization=0.8;spec_draft_length=0', '--enable-debug', '--host', '0.0.0.0', '--port', '8000'])
+subprocess.run(['python', '-m', 'mlc_llm', 'serve', 
+                '/Llama-3.1-8B-Instruct-q4f16_1-MLC', 
+                '--model-lib', '/Llama-3.1-8B-Instruct-q4f16_1-MLC/lib_disagg.so', 
+                '--device', f'cuda:{cuda_id}', 
+                '--enable-debug', 
+                '--mode', 'server', 
+                '--speculative-mode', 'disable', 
+                '--prefix-cache-mode', 'disable', 
+                '--overrides', 'gpu_memory_utilization=0.8;spec_draft_length=0', 
+                '--host', '0.0.0.0', 
+                '--port', '8000'])
--- services/serve/serve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python -m mlc_llm compile Llama-3.1-8B-Instruct-q4f16_1-MLC/ \
-  --opt O3 \
-  --overrides "disaggregation=1" \
-  -o Llama-3.1-8B-Instruct-q4f16_1-MLC/lib_disagg.so
-
-python serve.py
